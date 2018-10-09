b690acfb39783411c4b7dbf40cf84a4523b57ba9
ethanliaw16 <ethan.liaw8@gmail.com>
2018-10-09 18:42:18 -0400

    Small changes to server

--- plan-your-life/server.js
+++ plan-your-life/server.js
@@ -1,6 +1,8 @@
 var http = require('http');
 
 http.createServer(function(req, res){
+  console.log("Server initialized.");
   res.writeHead(200, {'Content-Type' : 'text/html'});
   res.end('PlanYourLife');
+
 }).listen(8000);
