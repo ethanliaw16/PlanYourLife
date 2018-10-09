d9b80fda9ee8aa284126080cbc30d638002171ed
ethanliaw16 <ethan.liaw8@gmail.com>
2018-10-09 18:38:51 -0400

    Put server file in correct place, cleaned up html for main page

--- plan-your-life/server.js
+++ plan-your-life/server.js
@@ -1,7 +1,6 @@
 var http = require('http');
-var datetime = require('./myFirstModule');
 
 http.createServer(function(req, res){
   res.writeHead(200, {'Content-Type' : 'text/html'});
   res.end('PlanYourLife');
-}).listen(8080);
+}).listen(8000);
