8ca1ca714767cee2ad6fc0b0ce2a9dba3e135d46
alankao64 <38641866+alankao64@users.noreply.github.com>
2018-11-01 15:22:59 -0400

    Calendar stuff
    minor change

--- calendar/index.js
+++ calendar/index.js
@@ -119,13 +119,13 @@ function listEvents(auth) {
     if (err) return console.log('The API returned an error: ' + err);
     const events = res.data.items;
     if (events.length) {
-      console.log('Upcoming 10 events:');
+      //console.log('Upcoming 10 events:');
       events.map((event, i) => {
         const start = event.start.dateTime || event.start.date;
         console.log(`${start} - ${event.summary}`);
       });
     } else {
-      console.log('No upcoming events found.');
+      //console.log('No upcoming events found.');
     }
   });
 }
