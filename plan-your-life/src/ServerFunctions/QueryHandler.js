a1e400e658266921cf6fb21c7cb4c1a3caf65511
Liaw <ethan.liaw8@gmail.com>
2018-11-15 14:09:28 -0500

    modified console output to accomodate kwi returning objects

--- plan-your-life/src/ServerFunctions/QueryHandler.js
+++ plan-your-life/src/ServerFunctions/QueryHandler.js
@@ -6,7 +6,28 @@ var CalendarEventInsert = require('./CalendarEventInsert');
 function processQuery(query, res){
     console.log('Reached the query handler. ' + query.text);
     var response = Kwi.KWI(query.text);
-    console.log('Result of Key word identifier on query: ' + response);
+    //JSON.parse(response);
+    console.log(String.format('Result of Key word identifier on query: {0} {1} {2} {3} {4} {5} {6} {7} {8} {9} ',
+     response.add,
+     response.download,
+     response.remind,
+     response.remove,
+     response.destination,
+     response.item,
+     response.time,
+     response.where,
+     response.event,
+     response.duration));
 
     var googleresponse = CalendarEventInsert.execute();
+  }
+
+  String.format = function(format) {
+    var args = Array.prototype.slice.call(arguments, 1);
+    return format.replace(/{(\d+)}/g, function(match, number) { 
+      return typeof args[number] != 'undefined'
+        ? args[number] 
+        : match
+      ;
+    });
   }
